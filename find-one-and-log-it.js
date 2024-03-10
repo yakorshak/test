a9f942ffe73b57086e2083a41520efc51bd4a7eb
Andrii Korshak <yakorshak@gmail.com>
2024-03-10 14:36:39 -0400

    version-1 create findOneAndLog function

---
+++ find-one-and-log-it.js
@@ -0,0 +1,8 @@
+export function findOneAndLogIt() {
+  const array = [1, "", 23, 1, "jdsafj"];
+  const newArray = array.filter((element) => element === 1);
+
+  console.log(newArray);
+}
+
+findOneAndLogIt();
